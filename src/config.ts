171e2793bdb7e8195d6e25dca674c6564b743f7f
Nathan Flurry <git@nathanflurry.com>
2023-10-14 23:25:27 +0000

    Rename RIVET_ORIGIN_API -> RIVET_API_ENDPOINT

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@
 // Provide default env if env variable is not defined. Any uses of `ENV.ABC` will be
 // overridden because of the WebPack `DefinePlugin`, so these act as defaults.
 
-const RIVET_ORIGIN_API = localStorage.RIVET_ORIGIN_API || ENV_RIVET_ORIGIN_API;
+const RIVET_API_ENDPOINT = localStorage.RIVET_API_ENDPOINT || ENV_RIVET_API_ENDPOINT;
 
 const config = {
 	RIVET_NAMESPACE: ENV_RIVET_NAMESPACE,
@@ -13,19 +13,19 @@ const config = {
 	GIT_COMMIT: ENV_GIT_COMMIT,
 	GIT_BRANCH: ENV_GIT_BRANCH,
 
-	ORIGIN_API: RIVET_ORIGIN_API as string,
+	ORIGIN_API: RIVET_API_ENDPOINT as string,
 	ASSETS_URL: ENV_ASSETS_URL as string,
 
-	MEDIA_URL: RIVET_ORIGIN_API + "/media",
+	MEDIA_URL: RIVET_API_ENDPOINT + "/media",
 
-	API_AUTH_URL: modifyBaseUrl(RIVET_ORIGIN_API, 'auth.', '/v1'),
-	API_PORTAL_URL: modifyBaseUrl(RIVET_ORIGIN_API, 'portal.', '/v1'),
-	API_IDENTITY_URL: modifyBaseUrl(RIVET_ORIGIN_API, 'identity.', '/v1'),
-	API_GROUP_URL: modifyBaseUrl(RIVET_ORIGIN_API, 'group.', '/v1'),
-	API_CHAT_URL: modifyBaseUrl(RIVET_ORIGIN_API, 'chat.', '/v1'),
-	API_CLOUD_URL: modifyBaseUrl(RIVET_ORIGIN_API, 'cloud.', '/v1'),
-	API_KV_URL: modifyBaseUrl(RIVET_ORIGIN_API, 'kv.', '/v1'),
-	API_PARTY_URL: modifyBaseUrl(RIVET_ORIGIN_API, 'party.', '/v1'),
+	API_AUTH_URL: modifyBaseUrl(RIVET_API_ENDPOINT, 'auth.', '/v1'),
+	API_PORTAL_URL: modifyBaseUrl(RIVET_API_ENDPOINT, 'portal.', '/v1'),
+	API_IDENTITY_URL: modifyBaseUrl(RIVET_API_ENDPOINT, 'identity.', '/v1'),
+	API_GROUP_URL: modifyBaseUrl(RIVET_API_ENDPOINT, 'group.', '/v1'),
+	API_CHAT_URL: modifyBaseUrl(RIVET_API_ENDPOINT, 'chat.', '/v1'),
+	API_CLOUD_URL: modifyBaseUrl(RIVET_API_ENDPOINT, 'cloud.', '/v1'),
+	API_KV_URL: modifyBaseUrl(RIVET_API_ENDPOINT, 'kv.', '/v1'),
+	API_PARTY_URL: modifyBaseUrl(RIVET_API_ENDPOINT, 'party.', '/v1'),
 
 	COMMUNITY_URL: 'https://discord.com/invite/aXYfyNxYVn',
 	DOCUMENTATION_URL: 'https://rivet.gg/docs',
