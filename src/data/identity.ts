eae7da249b8c8a614a3bb13ff0fe1086ed8e2246
Nathan Flurry <git@nathanflurry.com>
2023-10-26 21:06:33 +0000

    Remove unused links

--- src/data/identity.ts
+++ src/data/identity.ts
@@ -4,6 +4,3 @@ export function padAccountNumber(n: number | string): string {
 	return ('0000' + n).slice(-4);
 }
 
-export function identityRouteData(data: api.identity.IdentityHandle): { id: string } {
-	return { id: data.identityId };
-}
