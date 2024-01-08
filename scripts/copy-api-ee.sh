abf53da68f267cd9c510e74d485eb6f6ca865d03
Nathan Flurry <git@nathanflurry.com>
2024-01-08 22:10:53 +0000

    Analytics overview re-enabled (#62)
    Not intended to be launched publicly yet, but it can be accessed if you know the URL.

--- scripts/copy-api-ee.sh
+++ scripts/copy-api-ee.sh
@@ -2,5 +2,9 @@
 set -euf
 
 cp  "$RIVET_EE_PATH/sdks/typescript/archive.tgz" vendor/rivet-gg-api-ee.tgz
+
+# HACK: Workaround to ensure the integrity hash is updated
+rm -rf node_modules
+rm yarn.lock
 yarn install
 
