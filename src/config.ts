6bac59fdf0fce8fc248b7ed03524dbb6e1a96d01
Nathan Flurry <developer@nathanflurry.com>
2024-06-05 00:51:11 -0700

    chore: fix old.rivet.gg

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@
 
 let apiEndpoint = localStorage.RIVET_API_ENDPOINT || ENV_RIVET_API_ENDPOINT;
 if (apiEndpoint == '__AUTO__') {
-	if (location.hostname == 'old.rivet.gg') {
+	if (location.hostname == 'old.rivet.gg' || location.hostname == 'old.hub-3av.pages.dev') {
 		apiEndpoint = 'https://api.rivet.gg';
 	} else if (location.hostname.startsWith('hub.')) {
 		// Connect to the corresponding API endpoint
