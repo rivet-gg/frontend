2613de1abed23e888a7cd37aeecf248966b6ed27
jog1t <39823706+jog1t@users.noreply.github.com>
2024-07-14 18:27:11 +0000

    fix(HUB-436): hide create team button if not admin (#233)
    closes HUB-436

--- apps/hub/src/domains/group/views/group-list-view.tsx
+++ apps/hub/src/domains/group/views/group-list-view.tsx
@@ -1,3 +1,4 @@
+import { useAuth } from "@/domains/auth/contexts/auth";
 import { gamesQueryOptions } from "@/domains/game/queries";
 import { Flex } from "@rivet-gg/components";
 import { useSuspenseQuery } from "@tanstack/react-query";
@@ -5,13 +6,14 @@ import { Group } from "../components/group";
 import { GroupCreateCard } from "../components/group-create-card";
 
 export function GroupListView() {
+  const { profile } = useAuth();
   const { data } = useSuspenseQuery(gamesQueryOptions());
   return (
     <Flex direction="col">
       {data.map((group) => (
         <Group key={group.groupId} {...group} />
       ))}
-      <GroupCreateCard />
+      {profile?.identity.isAdmin ? <GroupCreateCard /> : null}
     </Flex>
   );
 }
