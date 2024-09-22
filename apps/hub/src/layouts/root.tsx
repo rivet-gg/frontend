7c5ea4139213dd8ed857469d9d51e9362ef12936
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2024-09-22 19:21:19 +0200

    chore: remove links which navigates to old hub

--- apps/hub/src/layouts/root.tsx
+++ apps/hub/src/layouts/root.tsx
@@ -36,10 +36,6 @@ const Footer = () => {
   return (
     <footer className="text-muted-foreground bg-background p-4 text-center text-sm">
       <div className="container">
-        <Link href="https://old.rivet.gg" target="_blank" rel="noreferrer">
-          Old Version
-        </Link>{" "}
-        &bull;{" "}
         <Link href="https://rivet.gg/support" target="_blank" rel="noreferrer">
           Support
         </Link>
