96ed1bb627179b101c426206b513d264533d3755
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2024-09-25 15:35:49 +0200

    feat: favicon update

--- apps/hub/src/vite-env.d.ts
+++ apps/hub/src/vite-env.d.ts
@@ -3,3 +3,13 @@
 declare const __APP_GIT_BRANCH__: string;
 declare const __APP_GIT_COMMIT__: string;
 declare const __APP_RIVET_NAMESPACE__: string;
+
+declare module "vite-plugin-favicons-inject" {
+  import type { Plugin } from "vite";
+
+  export default function vitePluginFaviconsInject(
+    iconPath: string,
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    options: Record<string, any>,
+  ): Plugin;
+}
