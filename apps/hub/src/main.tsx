01045ecd95ce560005395d50186625a7a03f6ca9
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2024-11-20 21:25:11 +0100

    refactor: call bootstrap before running the app (w/o sdk update)

--- apps/hub/src/main.tsx
+++ apps/hub/src/main.tsx
@@ -3,16 +3,29 @@ import ReactDOM from "react-dom/client";
 import { App } from "./app";
 import "./index.css";
 import { initThirdPartyProviders } from "./components/third-party-providers";
+import { rivetClient } from "./queries/global";
 
 initThirdPartyProviders();
 
-// biome-ignore lint/style/noNonNullAssertion: it should always be present
-const rootElement = document.getElementById("root")!;
-if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
+rivetClient.cloud
+  .bootstrap()
+  .then((response) => {
+    // @ts-ignore
+    run({ cacheKey: [response.deploy_hash, __APP_GIT_COMMIT__].join("-") });
+  })
+  .catch(() => {
+    run();
+  });
+
+function run({ cacheKey }: { cacheKey?: string } = {}) {
+  // biome-ignore lint/style/noNonNullAssertion: it should always be present
+  const rootElement = document.getElementById("root")!;
+  if (!rootElement.innerHTML) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+      <StrictMode>
+        <App cacheKey={cacheKey} />
+      </StrictMode>,
+    );
+  }
 }
